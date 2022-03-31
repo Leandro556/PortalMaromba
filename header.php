c8fa57560a9f0c2c12ed283e633e64f6ff3dc2dd
Leandro556 <leandrobertocchi1@gmail.com>
2022-03-31 16:53:20 -0300

    Teste falho ❗️

---
+++ header.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Título</title>
+        <!-- Aqui também incluo scripts de css e js -->
+
+    </head>
+    <body>
+        <h1> ola header</h1> <br>
+        <h2> funcionando </h2> <br>
+        <h1> ola header</h1> <br>
+        <h2> funcionando </h2> <br>
+        <h1> ola header</h1> <br>
+        <h2> funcionando </h2> <br>
+        <h1> ola header</h1> <br>
+        <h2> funcionando </h2>
+</body>
