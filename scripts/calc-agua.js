2ba0f2bc5aa338ceb2f45cd1f9902e7983743f34
Leandro556 <leandrobertocchi1@gmail.com>
2022-09-16 15:34:30 -0300

    clean code

--- scripts/calc-agua.js
+++ scripts/calc-agua.js
@@ -1,7 +1,8 @@
-function calculadora() {  
-    var n1 = parseFloat(document.getElementById('n1').value); 
-    var n2 = 0.04   
- resultado = n1 * n2;
-   
-    document.getElementById('resultado').innerHTML = `Você precisa tomar ` + resultado + ` litros de água por dia`;  
- }  
+function calculadora() {
+  var n1 = parseFloat(document.getElementById("n1").value);
+  var n2 = 0.04;
+  resultado = n1 * n2;
+
+  document.getElementById("resultado").innerHTML =
+    `Você precisa tomar ` + resultado + ` litros de água por dia`;
+}
